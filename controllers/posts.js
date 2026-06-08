0d94dea9cc1a84a7a556a768d3afbbef1e0ef97a
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-06-08 16:04:11 +0200

    aggiunta query SQL per la lista dei post nell'index

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,26 +1,29 @@
+import connection from '../config/db.js';
+
 // Index - restituisce la lista di tutti i post
-const index = (request, response) => {
-  response.json({ message: 'Lista dei post' });
+const index = async (request, response) => {
+    const [posts] = await connection.query('SELECT * FROM posts');
+    response.json(posts);
 };
 
 // Show - restituisce un singolo post
 const show = (request, response) => {
-  response.json({ message: 'Dettaglio del post' });
+    response.json({ message: 'Dettaglio del post' });
 };
 
 // Store - crea un nuovo post
 const create = (request, response) => {
-  response.json({ message: 'Creazione di un nuovo post' });
+    response.json({ message: 'Creazione di un nuovo post' });
 };
 
 // Update - modifica un post esistente
 const update = (request, response) => {
-  response.json({ message: 'Modifica del post' });
+    response.json({ message: 'Modifica del post' });
 };
 
 // Destroy - cancella un post
 const destroy = (request, response) => {
-  response.json({ message: 'Cancellazione del post' });
+    response.json({ message: 'Cancellazione del post' });
 };
 
 export { index, show, create, update, destroy };
